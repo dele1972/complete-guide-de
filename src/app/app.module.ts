859a7c74643d3e2fc2cb0805fe5687fcf1144c18
lederich <lederich@hornetsecurity.com>
2018-11-06 12:18:59 +0100

    setup for 'empty project' to master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
-import { AnotherComponent } from './other/another.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OtherComponent,
-    AnotherComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
